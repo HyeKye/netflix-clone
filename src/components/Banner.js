98a98fa1c7526879a9e8df5fa1ad48282f6026a0
Jordan <jordan.k.wilia@gmail.com>
2022-10-12 12:47:00 -0700

    wip

---
+++ src/components/Banner.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+
+    function truncate(string, n) {
+        return string?.length > n ? string.substr(0, n - 1) + "..." : string
+    }
+
+  return (
+    <header className="banner" style={{
+        backgroundSize: "cover",
+        backgroundImage: `url('https://res.cloudinary.com/practicaldev/image/fetch/s--THrf5Yjw--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/n6brz4p7iq7j1mulo1nv.jpg')`,
+        backgroundPosition: 'center center',
+    }}>
+        <div className="banner_contents">
+            <h1 className="banner_title">Movie Name</h1>
+            <div className="banner_buttons">
+                <button className="banner_button">Play</button>
+                <button className="banner_button">My List</button>
+            </div>
+            <h1 className="banner_description">{truncate(`This is a test This is a test This is a test This is a test This is a test This is a test This is a test This is a test This is a test This is a test This is a test This is a test`, 150)}</h1>
+        </div>
+
+        <div className="banner--fadeBottom" />
+    </header>
+  )
+}
+
+export default Banner
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA2-DbXYRLqHcPxbd_kzbbP7EWKa8AqGEc",
+    authDomain: "netflix-clone-57cf0.firebaseapp.com",
+    projectId: "netflix-clone-57cf0",
+    storageBucket: "netflix-clone-57cf0.appspot.com",
+    messagingSenderId: "612368676128",
+    appId: "1:612368676128:web:664cae59808d8528039345"
+  };
